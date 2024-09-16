fff4268a62e4035df0f52101b6b70576936d75dd
Mohammed-Abubakar-11786 <mohdabubakar.11786@gmail.com>
2024-09-16 20:06:50 +0530

    s

--- server/app.js
+++ server/app.js
@@ -106,7 +106,8 @@ const SOCKET_PORT = process.env.SOCKET_PORT || 3031;
 const http = require("http").Server(app);
 const io = new Server(http, {
   cors: {
-    origin: "*" || "*", // Use environment variable or fallback to localhost
+    path: "/socket",
+    origin: process.env.CLIENT_ORIGIN || "*", // Use environment variable or fallback to localhost
     methods: ["GET", "POST"],
     credentials: true,
   },
