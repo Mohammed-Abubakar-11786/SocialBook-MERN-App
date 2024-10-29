d594507d18d45915758d11d4256e9ea8eb3d2d7e
Mohammed-Abubakar-11786 <mohdabubakar.11786@gmail.com>
2024-10-29 13:58:59 +0530

    added port 0.0.0.0

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0", // Bind to 0.0.0.0 for external connections
+    port: 5173, // Set the port as needed
+  },
+});
