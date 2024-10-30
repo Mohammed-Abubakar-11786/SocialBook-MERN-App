f1a59345f0382e83121d63a21bc2c0c970366abf
Mohammed-Abubakar-11786 <mohdabubakar.11786@gmail.com>
2024-10-30 15:06:18 +0530

    new27

--- server/app.js
+++ server/app.js
@@ -87,7 +87,7 @@ let sessionOptions = {
     expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    secure: false, // Set to true in production
+    secure: true, // Set to true in production
   },
 };
 app.use(session(sessionOptions));
