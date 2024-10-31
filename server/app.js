5b0ac6e4a2a6825408ceff97bd9339c84c8f4c3c
Mohammed-Abubakar-11786 <mohdabubakar.11786@gmail.com>
2024-10-31 21:38:53 +0530

    new37

--- server/app.js
+++ server/app.js
@@ -88,7 +88,7 @@ let sessionOptions = {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     secure: process.env.NODE_ENV == "production",
-    // sameSite: "strict", // Required for cross-site cookies
+    sameSite: "strict", // Required for cross-site cookies
     // domain: ".onrender.com", // Allows access on both subdomains
   },
 };
